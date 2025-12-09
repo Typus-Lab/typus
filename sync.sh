7ba7ecc579857b413d4ded24b15fa6546c713bb9
Wayne Kuo <wayne.k@typuslab.com>
2025-12-09 14:41:06 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,3 +2,4 @@ cp -fr ../typus-dov-private/typus_dov_single/ typus_dov_single
 cp -fr ../typus-dov-private/typus_framework/ typus_framework
 cp -fr ../typus-move/typus/ typus
 cp -fr ../typus-perp/typus_stake_pool/ typus_stake_pool
+cp -fr ../typus-oracle/oracle/ typus_oracle
